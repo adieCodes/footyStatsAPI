65ed69d85d3dfb47d54b0941f999625258d3477a
Adie Williams <hello@adie.codes>
2020-11-03 17:23:33 +0000

    Add query for season stats

--- src/stats/stats.spec.js
+++ src/stats/stats.spec.js
@@ -7,10 +7,51 @@ describe('# Stats', () => {
     connection.end();
   });
 
-  it('Should return 200', async () => {
-    const res = await request(app).get('/stats');
+  const exampleRes = {
+    period: 'week',
+    stats: [
+      {
+        teamName: expect.any(String),
+        savesTier2: expect.any(Number),
+        lastName: expect.any(String),
+        savesTier1: expect.any(Number),
+        subs: expect.any(Number),
+        motms: expect.any(Number),
+        points: expect.any(Number),
+        redCards: expect.any(Number),
+        concedes: expect.any(Number),
+        assists: expect.any(Number),
+        shotsTier1: expect.any(Number),
+        shotsTier2: expect.any(Number),
+        id: expect.any(Number),
+        starts: expect.any(Number),
+        goals: expect.any(Number),
+        tacklesTier1: expect.any(Number),
+        tacklesTier2: expect.any(Number),
+        ownGoals: expect.any(Number),
+        cleansheets: expect.any(Number),
+        penSaves: expect.any(Number),
+        firstName: expect.any(String),
+        penMisses: expect.any(Number),
+        passesTier1: expect.any(Number),
+        position: expect.any(String),
+        passesTier2: expect.any(Number),
+        yellowCards: expect.any(Number),
+      },
+    ],
+  };
 
-    expect(res.statusCode).toEqual(200);
+  describe('# /stats/season', () => {
+    it('Should return 200 and correct shape data', async () => {
+      const res = await request(app).get('/stats');
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.period).toEqual('season');
+      for (let i = 0; i < res.body.stats.length; i += 1) {
+        expect(res.body.stats[i]).toEqual(exampleRes.stats[0]);
+      }
+    });
   });
   // TODO: Add error handling
+  //   TODO: Clarify what id is in res
 });
--- src/stats/statsController.js
+++ src/stats/statsController.js
@@ -1,8 +1,43 @@
 const connection = require('../db/connection');
 
 module.exports = (req, res) => {
-  connection.query('select * from players', (err, results) => {
+  const seasonQuery = `SELECT
+    t.name as teamName,
+    sp.savesTier2,
+    p.lastName,
+    sp.savesTier1,
+    sp.subs,
+    sp.motms,
+    sp.points,
+    sp.redCards,
+    sp.concedes,
+    sp.assists,
+    sp.shotsTier1,
+    sp.shotsTier2,
+    sp.playerId as id,
+    sp.starts,
+    sp.goals,
+    sp.tacklesTier1,
+    sp.tacklesTier2,
+    sp.ownGoals,
+    sp.cleansheets,
+    sp.penSaves,
+    p.firstName,
+    sp.penMisses,
+    sp.passesTier1,
+    p.position,
+    sp.passesTier2,
+    sp.yellowCards
+    FROM practical.seasonPlayers sp
+    inner join players p on sp.playerId = p.id
+    inner join teams t on p.teamid = t.id;`;
+
+  connection.query(seasonQuery, (err, results) => {
     if (err) throw err;
-    res.send(JSON.stringify(results));
+    const resObj = {
+      period: 'season',
+      stats: results,
+    };
+    res.json(resObj);
   });
 };
