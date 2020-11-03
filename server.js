e33b606dee8437273482e931ac058a0456449492
Adie Williams <hello@adie.codes>
2020-11-03 19:43:25 +0000

    Disable eslint for logging when using app.listen

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ const url = process.env.URL || 'http://localhost';
 
 app.set('port', port);
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`App is listening at ${url}:${port}`));
