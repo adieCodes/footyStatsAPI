0ac1adc0525bcc7b3d56c27f58459e90795b3528
Adie Williams <hello@adie.codes>
2020-11-10 08:03:14 +0000

    Remove unneccessary setting of port

--- server.js
+++ server.js
@@ -3,7 +3,5 @@ const app = require('./app');
 const port = process.env.PORT || 3001;
 const url = process.env.URL || 'http://localhost';
 
-app.set('port', port);
-
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`App is listening at ${url}:${port}`));
